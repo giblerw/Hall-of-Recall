5568e9701f442c8e68e2b2d9ca0d5c606173f095
Weston Gibler <westongibler@gmail.com>
2017-10-23 13:37:52 -0600

    HBS set

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var knex = require('knex');
 var pg = require('pg');
 
+
+//routes
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -16,6 +18,27 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req,res) => {
+  res.render('auth');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Api listening on port 3000`);
