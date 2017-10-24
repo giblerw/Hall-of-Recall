87a3de22b6aa8ccb9b98ed47bb7ce44ae41c8a5c
Clark Walker <303walker@gmail.com>
2017-10-24 14:40:18 -0600

    test

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../db/knex');
+
+const express = require('express')
+const router = express.Router()
+const db = require('../db/knex')
 
 
-router.get('/', async, (req, res, next) => {
-  const Users = await db('users');
-  console.log(Users);
-  res.json(Users);
+router.get('/', async (req, res, next) => {
+  const Users = await db('users')
+console.log(Users);
+  res.json(Users)
+
 });
 
-module.exports = router;
+module.exports = router
