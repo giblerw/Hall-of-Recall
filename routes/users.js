2ad3b83b1e01b332229107510b2628300c081721
Weston Gibler <westongibler@gmail.com>
2017-10-24 10:46:18 -0600

    users db setup

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,13 @@
-var express = require('express');
-var router = express.Router();
+
+const express = require('express')
+const router = express.Router()
 const db = require('../db/knex')
-//API login
-const okta = require('@okta/okta-sdk-nodejs');
-const client = new okta.Client({
-  orgUrl: 'dev-411294.oktapreview.com',
-  token: '00sUprUO1DpiBRnYzz83s6wxlZyKdh8mYVO9-WGjkY'
+
+router.get('/', async (req, res, next) => {
+  const Users = await db('users')
+console.log(Users);
+  res.json(Users)
+
 });
-const newUser = {
-  profile: {
-    firstName: 'Foo',
-    lastName: 'Bar',
-    email: 'foo@example.com',
-    login: 'foo@example.com',
-  },
-  credentials: {
-    password : {
-      value: 'PasswordAbc123'
-    }
-  }
-};
 
-//Create User
-// client.createUser(newUser)
-// .then(user => {
-//   console.log('Created user', user);
-// });
-
-//Read user
-
-// client.getUser('foo@bar.com').then(user => {
-//   console.log(user);
-// });
-module.exports = router;
+module.exports = router
