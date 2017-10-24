e59d48a83f124d744d218f899cda7eb84643e22b
Clark Walker <303walker@gmail.com>
2017-10-24 14:17:19 -0600

    bit of linting and indenting

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/knex');
 
-const express = require('express')
-const router = express.Router()
-const db = require('../db/knex')
 
-
-router.get('/', async (req, res, next) => {
-  const Users = await db('users')
-console.log(Users);
-  res.json(Users)
-
+router.get('/', async, (req, res, next) => {
+  const Users = await db('users');
+  console.log(Users);
+  res.json(Users);
 });
 
-module.exports = router
+module.exports = router;
