1c2d0933f7e7bfd384f6504a9a93a9f928907431
Weston Gibler <westongibler@gmail.com>
2017-10-24 10:52:17 -0600

    update

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db/knex')
-//API login
-const okta = require('@okta/okta-sdk-nodejs');
-const client = new okta.Client({
-  orgUrl: 'dev-411294.oktapreview.com',
-  token: '00sUprUO1DpiBRnYzz83s6wxlZyKdh8mYVO9-WGjkY'
-});
-const newUser = {
-  profile: {
-    firstName: 'Foo',
-    lastName: 'Bar',
-    email: 'foo@example.com',
-    login: 'foo@example.com',
-  },
-  credentials: {
-    password : {
-      value: 'PasswordAbc123'
-    }
-  }
-};
+
 
 //Create User
 // client.createUser(newUser)
