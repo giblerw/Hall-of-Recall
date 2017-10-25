933eba55cdc6f52fa310cfe1fe80263f9b07aed4
Clark Walker <303walker@gmail.com>
2017-10-25 15:44:32 -0600

    merge that shit

--- public/game.js
+++ public/game.js
@@ -33,16 +33,20 @@ function update() {
   player.body.velocity.x = 0;
 
   if (cursors.left.isDown) {
-    player.body.velocity.x = -150;
     player.animations.play('left');
   } else if (cursors.right.isDown) {
-    player.body.velocity.x = 150;
     player.animations.play('right');
   } else {
     player.animations.stop();
     player.frame = 4;
   }
 
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -150;
+  }
+  if (cursors.right.isDown) {
+    player.body.velocity.x = 150;
+  }
 // Define hitPlatform DO ET
   if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
     player.body.velocity.y = -350;
