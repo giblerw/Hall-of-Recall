9c77922102154462667571534f0356778aff36d1
Clark Walker <303walker@gmail.com>
2017-10-26 11:18:07 -0600

    update

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,11 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/hallOfRecall'  
+    connection: 'postgres://localhost/hallOfRecall'
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
-
   }
 
 };
